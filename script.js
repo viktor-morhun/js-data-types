5efe5a2d7ed9759d05891c865989846c76b6f041
Viktor Morhun <morhun.fe@gmail.com>
2024-07-30 15:00:46 +0300

    task 1.1 - 1.3 completed

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 'use strict';
+
+function getNumbers () {
+
+};
+
+//Task 1.1
+function addNumbers (firstNumber = 0.1, secondNumber = 0.2) {
+  return (firstNumber + secondNumber).toFixed(1);
+};
+
+console.log(addNumbers());
+
+
+//Task 1.2
+function addStringNumbers (firstNumber = "1", secondNumber = 2) {
+  return (Number(firstNumber) + secondNumber);
+};
+
+console.log(addStringNumbers());
+
+
+// Task 1.3
+let flashDriveSize = '';
+function getFileSize () {
+  flashDriveSize = prompt('Enter flash drive size in Gb');
+  flashDriveSize *= 1024;
+};
+
+function countFiles (fileSize = 820, storageSize) {
+  return Math.floor(storageSize/fileSize);
+};
+
+getFileSize();
+console.log(countFiles(820, flashDriveSize));
+
+
+//Task 2.1
+
